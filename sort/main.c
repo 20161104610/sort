34b529e589e9a4bdde6def3744c549b88e5dbec1
Stu06Han <2357706458@qq.com>
2017-06-12 10:47:01 +0800

    Initial Commit

---
+++ sort/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  sort
+//
+//  Created by 20161104610 on 17/6/12.
+//  Copyright © 2017年 20161104610. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
