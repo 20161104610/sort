823c24226d42538a9a98087f1adeb888e1ed0f1f
Stu06Han <2357706458@qq.com>
2017-06-12 11:46:02 +0800

    sort

--- sort/main.c
+++ sort/main.c
@@ -8,8 +8,43 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+int main()
+{
+    int i;
+    int j;
+    int m;
+    int a[9];
+    int b[9];
+    
+    printf("请输入任意10个数字：\n");
+    
+    i=0;
+    j=0;
+    m=0;
+    
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    
+    for(i=0;i<10;i++)
+    {
+        for(j=0;j<10-i;j++)
+        {
+            if(a[i]>=a[i+1])
+            {
+                a[i]=m;
+                a[i]=a[i+1];
+                a[i+1]=m;
+            }
+            i++;
+        }
+    }
+    
+    for(i=0;i<10;i++)
+    {
+        b[i]=a[i];
+        printf("%d ",b[i]);
+    }
+    
 }
