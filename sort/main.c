a8e648f8a7cdc74138db0eeb5cbe2f93c319ef19
Stu06Han <2357706458@qq.com>
2017-06-13 09:30:34 +0800

    改.1
    1

--- sort/main.c
+++ sort/main.c
@@ -13,38 +13,58 @@ int main()
     int i;
     int j;
     int m;
+    int n;
+    int k;
     int a[9];
-    int b[9];
+    int b[10];
     
-    printf("请输入任意10个数字：\n");
+    FILE *fr;
+    FILE *fw;
     
+    fr=fopen("/Users/a201611046101/Desktop/sort/sort.txt","r+");
+    fw=fopen("/Users/a201611046101/Desktop/sort/output.txt","w");
+
     i=0;
     j=0;
     m=0;
+    n=0;
+    k=0;
+    
+    printf("请输入任意10个数字：\n");
     
     for(i=0;i<10;i++)
     {
         scanf("%d",&a[i]);
-    }
-    
-    for(i=0;i<10;i++)
-    {
-        for(j=0;j<10-i;j++)
-        {
-            if(a[i]>=a[i+1])
-            {
-                a[i]=m;
-                a[i]=a[i+1];
-                a[i+1]=m;
-            }
-            i++;
-        }
+        fprintf(fr,"%d ",a[i]);
     }
     
     for(i=0;i<10;i++)
     {
         b[i]=a[i];
-        printf("%d ",b[i]);
     }
+    
+    n=b[0];
+    
+    for(i=0;i<10;i++)
+    {
+        for(j=0;j<10-i;j++)
+        {
+            if(b[j]>=b[j+1])
+            {
+                m=b[j];
+                b[j]=b[j+1];
+                b[j+1]=m;
+            }
+        }
+    }
+
+    for(i=0;i<10;i++)
+    {
+        a[i]=b[i+1];
+        printf("%d ",a[i]);
+        fprintf(fw,"%d ",a[i]);
+    }
+    
+    printf("\n");
     
 }
